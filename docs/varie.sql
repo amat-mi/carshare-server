288bbdd9d5b458d201883bbdea8da640c9676eb8
caipirginka <github@oicom.com>
2016-01-29 19:09:10 +0100

    Aggiunto SQL per contare campioni presenti in ogni intervallo di 5 minuti.

--- docs/varie.sql
+++ docs/varie.sql
@@ -14,6 +14,7 @@ group by vehicle_id
 having count(*) >= 5 order by count(*) desc)
 order by vehicle_id,stamp;
 
+
 /**
 	Calcola la distanza in metri e in gradi tra due record di dati
 	(tipicamente dovrebbero essere due dati consecutivi dello stesso veicolo).
@@ -26,3 +27,34 @@ st_asewkt(d1.geom),st_asewkt(d2.geom),
 *
 from carshare_vehicledata d1,carshare_vehicledata d2
 where d1.id=12105 and d2.id=13267;
+
+
+/** 
+	Calcola il numero di campioni presenti in ogni intervallo di 5 minuti.
+	ATTENZIONE!!! Ignora eventuali campioni con i minuti di "stamp" non allineati
+ 	ai 5 minuti, ma non dovrebbero essercene!!!
+*/
+
+select
+d.stamp::date,
+extract(hour from d.stamp) as hour,
+s.m as minute,
+count(*)
+from carshare_vehicledata d
+join (select generate_series(2,57,5) as m) s on s.m=extract(minute from d.stamp)
+group by d.stamp::date,extract(hour from d.stamp),s.m
+order by d.stamp::date,extract(hour from d.stamp),s.m;
+
+
+/**
+	Seleziona i campioni con i minuti di "stamp" non allineati ai 5 minuti.
+	Non dovrebbero essercene e invece ci sono, ma pochi, a parte i primi campioni
+	memorizzati durante i test...
+*/
+
+select
+*
+from carshare_vehicledata d
+left join (select generate_series(2,57,5) as m) s on s.m=extract(minute from d.stamp)
+where s.m is null
+order by stamp desc;
