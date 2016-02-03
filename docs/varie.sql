34604219e7950693afa8279cc230a2e6d32055d3
caipirginka <github@oicom.com>
2016-02-03 18:25:26 +0100

    Aggiunto SQL per materializzare veicoli effettivamente presenti in ogni intervallo di 5 minuti.

--- docs/varie.sql
+++ docs/varie.sql
@@ -30,18 +30,17 @@ where d1.id=12105 and d2.id=13267;
 
 
 /** 
-	Calcola il numero di campioni presenti in ogni intervallo di 5 minuti.
-	ATTENZIONE!!! Ignora eventuali campioni con i minuti di "stamp" non allineati
- 	ai 5 minuti, ma non dovrebbero essercene!!!
+	Calcola il numero di campioni presenti in ogni intervallo di 5 minuti,
+	considerando anche quelli non eventualmente allineati ai 5 minuti.
 */
 
 select
 d.stamp::date,
 extract(hour from d.stamp) as hour,
-s.m as minute,
+s.m+2 as minute,
 count(*)
 from carshare_vehicledata d
-join (select generate_series(2,57,5) as m) s on s.m=extract(minute from d.stamp)
+join (select generate_series(0,55,5) as m) s on extract(minute from d.stamp) between s.m and s.m+4
 group by d.stamp::date,extract(hour from d.stamp),s.m
 order by d.stamp::date,extract(hour from d.stamp),s.m;
 
@@ -58,3 +57,79 @@ from carshare_vehicledata d
 left join (select generate_series(2,57,5) as m) s on s.m=extract(minute from d.stamp)
 where s.m is null
 order by stamp desc;
+
+
+/**
+	Crea il dato completo per un certo veicolo in un certo periodo prendendo per ogni intervallo di 5 minuti
+	l'ultimo record esistente, anche se con dato non disponibile (veicolo in viaggio).
+*/
+
+WITH tick AS (
+select
+'2016-01-26 20:00:00'::timestamp + (s.m+2 || ' minutes')::interval as stamp,
+'2016-01-26 20:00:00'::timestamp + (s.m || ' minutes')::interval as stampfrom,
+'2016-01-26 20:00:00'::timestamp + (s.m+5 || ' minutes')::interval as stampto
+from (select generate_series(0,(24*60*7)-1,5) as m) s
+),
+d AS (
+select
+*,
+lead(stamp) OVER (PARTITION BY vehicle_id ORDER BY id) as stampto
+from carshare_vehicledata d
+)
+select
+d.*,
+ref.stamp as tickstamp
+from (
+select * from carshare_vehicle v, tick
+) ref
+left join d on d.vehicle_id=ref.id and 
+	(
+		(d.stamp>=ref.stampfrom and d.stamp<ref.stampto) or
+		(ref.stampfrom>=d.stamp and ref.stampto<d.stampto)
+	)
+where ref.id=1
+order by ref.stampfrom,ref.id;
+
+
+/**
+	Crea una tabella che materializza il dato completo per tutti i veicoli in un certo periodo 
+	prendendo per ogni intervallo di 5 minuti l'ultimo record esistente, 
+	ma solo se con dato disponibile (veicolo non in viaggio).
+	ATTENZIONE!!! E' un po' pesante (circa 4 minuti e mezzo per 2.150.155 record in un periodo di una settimana)!!!
+*/
+
+CREATE TABLE carshare_20160127_one_week AS
+WITH tick AS (
+select
+'2016-01-27 00:00:00'::timestamp + (s.m+2 || ' minutes')::interval as stamp,
+'2016-01-27 00:00:00'::timestamp + (s.m || ' minutes')::interval as stampfrom,
+'2016-01-27 00:00:00'::timestamp + (s.m+5 || ' minutes')::interval as stampto
+from (select generate_series(0,(24*60*7)-1,5) as m) s
+),
+d AS (
+select
+*,
+lead(stamp) OVER (PARTITION BY vehicle_id ORDER BY id) as stampto
+from carshare_vehicledata d
+
+)
+select
+ref.stamp as stamp,
+d.vehicle_id,
+d.address,
+d.fuel,
+d.inside,
+d.outside,
+d.webstamp,
+d.geom
+from (
+select * from carshare_vehicle v, tick
+) ref
+left join d on d.vehicle_id=ref.id and 
+	(
+		(d.stamp>=ref.stampfrom and d.stamp<ref.stampto) or
+		(ref.stampfrom>=d.stamp and ref.stampto<d.stampto)
+	)
+where webstamp is not null
+order by ref.stampfrom,ref.id;
